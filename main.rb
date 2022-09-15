ed7091e2ea5d434f8d2ac1934dd17f1a3caf1ffe
Joseph Petersen <joseph.j.petersen@gmail.com>
2022-09-15 09:33:06 -0400

    Create modules.rb and begin writing intro code in main.rb

---
+++ main.rb
@@ -0,0 +1,11 @@
+require_relative "./game.rb"
+require_relative "./modules.rb"
+include Clear
+
+def introduction
+  clear
+  puts "Welcome to ~ M A S T E R M I N D ~"
+end
+
+introduction
+
---
+++ modules.rb
@@ -0,0 +1,5 @@
+module Clear
+  def clear
+    print "\033[2J\033[H"
+  end
+end
